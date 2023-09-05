0c1976de861b96528e4aa032c474eb9d8ad3cd4f
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-09-05 10:41:53 +0530

    build issues fix

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
-import { useRouter } from "next/router";
 
 export default function Page() {
-	const router = useRouter();
-
 	return (
 		<div className="flex justify-center pt-12">
 			<SignUp />
